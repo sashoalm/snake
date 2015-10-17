41080957f84a8d0994d59041b2b1fef257157207
Alexander Almaleh <sashoalm@gmail.com>
2015-10-17 16:21:24 +0300

    Paint the widget.

--- widget.cpp
+++ widget.cpp
@@ -1,7 +1,31 @@
 #include "widget.h"
 
+#include <QPainter>
+
 Widget::Widget(QWidget *parent) : QWidget(parent)
 {
+    cells.resize(fieldWidth()*fieldHeight());
+    cells.fill(Empty);
 
+    // Draw the wall around the field.
+    for (int ii = 0; ii < fieldWidth(); ++ii) {
+        setCell(ii, 0, Wall);
+        setCell(ii, fieldHeight()-1, Wall);
+    }
+    for (int ii = 0; ii < fieldHeight(); ++ii) {
+        setCell(0, ii, Wall);
+        setCell(fieldWidth()-1, ii, Wall);
+    }
+}
+
+void Widget::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    Qt::GlobalColor cols[] = { Qt::blue, Qt::red, Qt::yellow, Qt::green };
+    for (int xx = 0; xx < fieldWidth(); ++xx) {
+        for (int yy = 0; yy < fieldHeight(); ++yy) {
+            painter.fillRect(xx*cellSize(), yy*cellSize(), cellSize(), cellSize(), cols[getCell(xx, yy)]);
+        }
+    }
 }
 
--- widget.h
+++ widget.h
@@ -12,6 +12,18 @@ public:
 signals:
 
 public slots:
+
+private:
+    virtual void paintEvent(QPaintEvent *);
+
+    int fieldWidth() { return 20; }
+    int fieldHeight() { return 20; }
+    int cellSize() { return 10; }
+
+    enum Cell { Empty, Wall, Food, Snake };
+    Cell getCell(int x, int y) { return cells[x+y*fieldWidth()]; }
+    void setCell(int x, int y, Cell val) { cells[x+y*fieldWidth()] = val; }
+    QVector<Cell> cells;
 };
 
 #endif // WIDGET_H
